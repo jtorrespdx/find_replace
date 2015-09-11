d0a98f1812ae6343b7419714487001c667cb5ef6
jose torres <josetorres@MacBook-Air.netgear.com>
2015-09-11 16:25:00 -0600

    adds two more tests, and passes. Fourth test not passing, will work on over the weekend (I'm out of town preparing for a wedding.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var findReplace = function(string, searchWord, replaceWord){
-
+    var searchWord = new RegExp(searchWord, 'gi')
     var replacePhrase = string.replace(searchWord, replaceWord);
 
     return replacePhrase;
--- spec/specs.js
+++ spec/specs.js
@@ -5,8 +5,16 @@ describe('findReplace', function() {
         expect(findReplace("tacos", "tacos", "taquitos")).to.equal("taquitos");
     })
 
-    // it("takes a phrase and replaces it with the same phrase", function() {
-    //     expect(findReplace("I like tacos")).to.equal("I like tacos");
+    it("takes a phrase and replaces it with the new phrase", function() {
+        expect(findReplace("I like tacos", "tacos", "taquitos")).to.equal("I like taquitos");
+    })
+
+    it("ignores mixed use of caps throughout the phrase", function () {
+        expect(findReplace("I like taCos", "tacos", "taquitos")).to.equal("I like taquitos");
+    })
+
+    // it("returns an error message if the search word is not found", function () {
+    //     expect(finReplace("I like tacos", "computer", "taquitos")).to.equal("Sorry, that word is not found.");
     // })
 
 
